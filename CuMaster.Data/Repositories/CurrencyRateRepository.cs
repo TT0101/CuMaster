08181417225c6a11d8311be686e90aaf9deb2426
TT0101 <invisiblecan@gmail.com>
2017-11-29 00:02:45 -0500

    Calling all calls from db instead of pulling all data for speed purposes (aka no master list; only get all when needed)

--- CuMaster.Data/Repositories/CurrencyRateRepository.cs
+++ CuMaster.Data/Repositories/CurrencyRateRepository.cs
@@ -6,26 +6,50 @@ using System.Text;
 using System.Threading.Tasks;
 using CuMaster.Data.Entities;
 using HelperFramework.Configuration;
+using System.Data.SqlClient;
 
 namespace CuMaster.Data.Repositories
 {
-    public class CurrencyRateRepository : RepositoryBase<CurrencyRateEntity>, ICurrencyRateRepository
+    public class CurrencyRateRepository : ICurrencyRateRepository
     {
         public CurrencyRateEntity GetSingle(string fromCurrencyCd, string toCurrencyCd)
         {
-            return this.EntityData.SingleOrDefault(e => e.FromCurrency == fromCurrencyCd && e.ToCurrency == toCurrencyCd);
-        }
-      
-        public IEnumerable<CurrencyRateEntity> GetRatesForCurrency(string currencyCd)
-        {
-            return this.EntityData.Where(e => e.FromCurrency == currencyCd || e.ToCurrency == currencyCd);
+            //return this.EntityData.SingleOrDefault(e => e.FromCurrency == fromCurrencyCd && e.ToCurrency == toCurrencyCd);
+            using (var context = new DataAccessFramework.DBConnection.DBConnectionContext(DatabaseName.CuMaster))
+            {
+                SqlParameter[] sparams = new SqlParameter[2];
+                sparams[0] = new SqlParameter("CurrencyFrom", fromCurrencyCd);
+                sparams[1] = new SqlParameter("CurrencyTo", toCurrencyCd);
+
+                return context.ExecuteSproc<CurrencyRateEntity>("usp_GetCurrencyRatesForPair", sparams).FirstOrDefault();
+            }
         }
 
-        internal override void RefreshData()
+        public IEnumerable<CurrencyRateEntity> GetRatesForCurrency(string currencyCd)
+        {
+            //return this.EntityData.Where(e => e.FromCurrency == currencyCd || e.ToCurrency == currencyCd);
+            using (var context = new DataAccessFramework.DBConnection.DBConnectionContext(DatabaseName.CuMaster))
+            {
+                SqlParameter[] sparams = new SqlParameter[1];
+                sparams[0] = new SqlParameter("Currency", currencyCd);
+
+                return context.ExecuteSproc<CurrencyRateEntity>("usp_GetCurrencyRatesUsingCurrency", sparams);
+            }
+        }
+
+        //internal override void RefreshData()
+        //{
+        //    using (var context = new DataAccessFramework.DBConnection.DBConnectionContext(DatabaseName.CuMaster))
+        //    {
+        //        this.EntityData = context.ExecuteSproc<CurrencyRateEntity>("usp_GetCurrencyRates");
+        //    }
+        //}
+
+        public IEnumerable<CurrencyRateEntity> Get()
         {
             using (var context = new DataAccessFramework.DBConnection.DBConnectionContext(DatabaseName.CuMaster))
             {
-                this.EntityData = context.ExecuteSproc<CurrencyRateEntity>("usp_GetCurrencyRates");
+                return context.ExecuteSproc<CurrencyRateEntity>("usp_GetCurrencyRates");
             }
         }
     }
